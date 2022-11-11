4af79263b3e06e5ed5c4d5909f8c0a673fadbdac
ibrohim021 <dim_aviasky@mail.ru>
2022-11-11 17:53:13 +0300

    bot

---
+++ bot.py
@@ -0,0 +1,9 @@
+#Здесь создается бот и хранится его токен
+
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+
+
+bot = Bot(token='5721421214:AAFi60iMSmFJhn3BQUxZ9zCbOLxUCVhGdwI')
+
+dp = Dispatcher(bot)
